58d108e1b97493c4f2eddcc2f8551fb287d56cb0
ninjatdx <59057819+ninjatdx@users.noreply.github.com>
2021-01-08 13:29:47 +0000

    updated mappings, improved smelting enchantment, changed textures, replaced logo file

--- src/main/java/com/formas1/ruispantry/enchantments/SmeltingEnchantment.java
+++ src/main/java/com/formas1/ruispantry/enchantments/SmeltingEnchantment.java
@@ -2,14 +2,18 @@ package com.formas1.ruispantry.enchantments;
 
 import com.formas1.ruispantry.RuisPantry;
 
+import net.minecraft.advancements.criterion.PlayerHurtEntityTrigger;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.IGrowable;
 import net.minecraft.client.Minecraft;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.GhastEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.HoeItem;
@@ -17,12 +21,17 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.living.LivingDestroyBlockEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
@@ -51,15 +60,34 @@ public class SmeltingEnchantment extends Enchantment
 	@Mod.EventBusSubscriber(modid = RuisPantry.MOD_ID, bus = Bus.FORGE)
 	public static class SmeltingEquipped
 	{
+		@SuppressWarnings("resource")
 		@SubscribeEvent
-		public static void doStuff(PlayerTickEvent event)
+		public static void holding(PlayerTickEvent event)
 		{
 			PlayerEntity player = event.player;
 			if(hasEnchantment(player.getHeldItemMainhand()))
 			{
-				if(player.world.getRandom().nextInt(5) == 0)
+				if(player.world.isRemote)
 				{
-					player.world.addParticle(ParticleTypes.FLAME, player.getPosX(), player.getPosY(), player.getPosZ(), 0, 0, 0);
+					if(player.world.getRandom().nextInt(5) == 0)
+					{
+						Vec3d vecLook = Minecraft.getInstance().objectMouseOver.getHitVec();
+						player.world.addParticle(ParticleTypes.FLAME, vecLook.getX(), vecLook.getY(), vecLook.getZ(), 0.0D, 0.0D, 0.0D);
+					}
+				}
+			}
+		}
+		
+		@SubscribeEvent
+		public static void hitMob(CriticalHitEvent event)
+		{
+			PlayerEntity player = event.getPlayer();
+			if(hasEnchantment(player.getHeldItemMainhand()))
+			{
+				if(event.getTarget() instanceof LivingEntity)
+				{
+					LivingEntity entity = (LivingEntity) event.getTarget();
+					entity.setFire(event.getEntity().world.rand.nextInt((8 - 5) + 5));
 				}
 			}
 		}
